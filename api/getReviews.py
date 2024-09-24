c34d8d6360145a8a1f5ed5cc1cddd858c5911cfe
IdeaMeet <11176762@qq.com>
2024-09-24 16:36:54 +0800

    test1636

--- api/getReviews.py
+++ api/getReviews.py
@@ -1,10 +1,9 @@
-# api/getReviews.py
+# api/hello.py
 
 import json
 
 def handler(request, response):
     try:
-        # 返回一个简单的 JSON 响应
         response.status_code = 200
         response.content_type = 'application/json'
         response.body = json.dumps({"message": "Hello, World!"})
